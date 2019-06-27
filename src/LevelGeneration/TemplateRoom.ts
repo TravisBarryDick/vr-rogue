95600dab4239cc423a8766c14248aa860e2294d0
TravisBarryDick <travis.barry.dick@gmail.com>
2019-06-27 14:32:40 -0600

    Added template room type

---
+++ src/LevelGeneration/TemplateRoom.ts
@@ -0,0 +1,85 @@
+import { Array2D } from "../Array2D";
+import { Tile } from "../Level";
+import { Room, isValidDoor } from "./LevelGeneration";
+import { RandomNumberGenerator } from "../RandomNumberGenerator";
+import { Rectangle } from "../Utils";
+
+export function parseTemplateRoom(room: string): TemplateRoom {
+  let lines = room.split("\n").filter(s => s.length > 0);
+  const height = lines.length;
+  const width = lines[0].length;
+  let tiles = new Array2D<Tile>(height, width, Tile.Empty);
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const char = lines[y][x];
+      if (char === "#") {
+        tiles.set(y, x, Tile.Wall);
+      } else if (char === ".") {
+        tiles.set(y, x, Tile.Floor);
+      } else if (char === "=") {
+        tiles.set(y, x, Tile.Door);
+      }
+    }
+  }
+  return new TemplateRoom(tiles);
+}
+
+export class TemplateRoom implements Room {
+  constructor(private tiles: Array2D<Tile>) {}
+
+  height() {
+    return this.tiles.height;
+  }
+
+  width() {
+    return this.tiles.width;
+  }
+
+  canPlaceAt(tiles: Array2D<Tile>, py: number, px: number): boolean {
+    // Rules for TemplateRoom placement:
+    // 1. Cannot place any tile on an existing floor or door tile.
+    // 2. At least one door must be a valid door.
+    const roomRect = new Rectangle(0, 0, this.height(), this.width());
+    let anyValidDoor = false;
+    for (let c of roomRect.areaCoords()) {
+      const levelTile = tiles.get(c.y + py, c.x + px);
+      const roomTile = this.tiles.get(c.y, c.x);
+      if (roomTile !== Tile.Empty) {
+        if (levelTile === Tile.Floor || levelTile === Tile.Door) return false;
+      }
+      if (roomTile === Tile.Door) {
+        if (isValidDoor(tiles, c.y + py, c.x + px)) anyValidDoor = true;
+      }
+    }
+    return anyValidDoor;
+  }
+
+  placeAt(
+    rng: RandomNumberGenerator,
+    tiles: Array2D<Tile>,
+    py: number,
+    px: number
+  ): void {
+    const roomRect = new Rectangle(0, 0, this.height(), this.width());
+    // Place walls and doors
+    for (let c of roomRect.areaCoords()) {
+      const roomTile = this.tiles.get(c.y, c.x);
+      if (roomTile === Tile.Wall) tiles.set(c.y + py, c.x + px, roomTile);
+      if (roomTile === Tile.Door) {
+        if (isValidDoor(tiles, c.y + py, c.x + px)) {
+          tiles.set(c.y + py, c.x + px, roomTile);
+        } else {
+          tiles.set(c.y + py, c.x + px, Tile.Wall);
+        }
+      }
+    }
+    // Place floors (need to do this second so that doors aren't validated by
+    // this tile's floors)
+    for (let c of roomRect.areaCoords()) {
+      const roomTile = this.tiles.get(c.y, c.x);
+      if (roomTile == Tile.Floor) {
+        tiles.set(c.y + py, c.x + px, roomTile);
+      }
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,30 @@ import { ComponentDefinition } from "aframe";
 import { Tile, Level } from "./Level";
 import { LevelGenerator } from "./LevelGeneration/LevelGeneration";
 import { RectangleRoomGenerator } from "./LevelGeneration/RectangularRoom";
+import { parseTemplateRoom } from "./LevelGeneration/TemplateRoom";
 import { LCG } from "./RandomNumberGenerator";
+
+const tRoom = parseTemplateRoom(`
+####=####
+=.......=
+####.####
+%%%#.#%%%
+%%%#.#%%%
+%%%#.#%%%
+%%%#=#%%%
+`);
+
+const oRoom = parseTemplateRoom(`
+%%%#=#%%%
+%%#...#%%
+%#.....#%
+#.......#
+=.......=
+#.......#
+%#.....#%
+%%#...#%%
+%%%#=#%%%
+`);
 
 interface RogueLevel {
   level: Level;
@@ -15,11 +38,13 @@ interface RogueLevel {
 
 AFRAME.registerComponent("rogue-level", <ComponentDefinition<RogueLevel>>{
   init: function() {
-    let generator = new LevelGenerator(50, 50, 10, [
-      RectangleRoomGenerator(4, 10, 4, 10)
-    ]);
+    let generator = new LevelGenerator(50, 50, 15, [
+      RectangleRoomGenerator(4, 10, 4, 10),
+      () => tRoom,
+      () => oRoom
+    ], [3, 1, 1]);
     const seed = Math.floor(Math.random() * 1000);
-    this.level = generator.generate(new LCG(seed));
+    this.level = generator.generate(new LCG(seed), true);
     this.createGeometry();
   },
 
@@ -33,6 +58,7 @@ AFRAME.registerComponent("rogue-level", <ComponentDefinition<RogueLevel>>{
           wallBox.setAttribute("height", "2");
           wallBox.setAttribute("material", "src", "#wall");
           wallBox.setAttribute("repeat", "1 2");
+          wallBox.setAttribute("roughness", 1);
           this.el.appendChild(wallBox);
         } else if (tile === Tile.Floor || tile === Tile.Door) {
           let floorPlane = document.createElement("a-plane");
@@ -40,6 +66,7 @@ AFRAME.registerComponent("rogue-level", <ComponentDefinition<RogueLevel>>{
           floorPlane.setAttribute("rotation", "-90 0 0");
           //floorPlane.setAttribute("material", "src", "#floor");
           floorPlane.setAttribute("color", "brown");
+          floorPlane.setAttribute("roughness", 1);
           this.el.appendChild(floorPlane);
         }
       }
